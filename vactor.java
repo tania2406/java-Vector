2412e91f31ff72d44af413533f0af38b65b20328
tania <taniaprasad4@gamil.com>
2024-05-29 22:09:40 +0530

    Vector

---
+++ vactor.java
@@ -0,0 +1,18 @@
+package DSA;
+
+import java.util.Vector;
+
+public class vactor 
+{
+	public static void main(String args[])
+	{
+		Vector<Integer> v=new Vector<Integer> ();
+		v.addElement(90);
+		v.addElement(78);
+		v.addElement(21);
+		v.addElement(67);
+		v.addElement(99);
+		System.out.println(v);
+	}
+
+}
